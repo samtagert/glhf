13c4e75a36b82d575e3c55302bbfec73be3e3d18
Sam Tagert <samtagert@gmail.com>
2017-10-30 20:51:25 -0700

    index page visual upgrade

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,16 @@ class Game < ApplicationRecord
   has_many :reviews
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def review_avg
+    @avg = 0
+    self.reviews.each do |review|
+      @avg += review.rating
+    end
+    if self.reviews.count > 0
+    @avg / self.reviews.count
+    else
+      @avg = 'N/A'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.destroy_all
 Game.destroy_all
 
 games = Game.create([
-  {name: "Jim's Jittery Java", release_date: "Medium", platform: "Bogota, Colombia"},
-  {name: "Jon's Coldest of Brews", release_date: "Mind Bending", platform: "Lagos, Nigeria"}
+  {name: "Super Mario Odyssey", release_date: 20171028, img: "Nintendo Switch", trailer: "x"},
+  {name: "Dark Souls 3", release_date: 20160324, img: "PC, PS4, Xbox", trailer: "x"},
+  {name: "Overwatch", release_date: 20150519, img: "PC, PS4, Xbox", trailer: "x"},
+  {name: "Shovel Knight", release_date: 20150519, img: "PC, Nintendo Switch", trailer: "x"},
+  {name: "Devil May Cry 4", release_date: 20150519, img: "PC, PS4, Xbox", trailer: "x"},
+  {name: "The Legend of Zelda", release_date: 20150519, img: "Nintendo 64", trailer: "x"},
+  {name: "Persona 5", release_date: 20150519, img: "PS4", trailer: "x"},
+  {name: "Metroid Prime", release_date: 20150519, img: "Gamecube", trailer: "x"},
+  {name: "Mark of the Ninja", release_date: 20150519, img: "PC", trailer: "x"},
+  {name: "Bioshock", release_date: 20150519, img: "PC, PS4, Xbox", trailer: "x"},
+  {name: "The Witcher 3", release_date: 20150519, img: "PC, PS4, Xbox", trailer: "x"},
+  {name: "Star Fox 64", release_date: 20150519, img: "Nintendo 64", trailer: "x"},
+  {name: "DOOM", release_date: 20150519, img: "PC, PS4, Xbox, Nintendo Switch", trailer: "x"},
+  {name: "Hearthstone", release_date: 20150519, img: "PC", trailer: "x"},
+  {name: "Halo 2", release_date: 20150519, img: "Xbox", trailer: "x"},
+  {name: "Final Fantasy VII", release_date: 20150519, img: "PC", trailer: "x"},
+  {name: "Mario Kart 8", release_date: 20150519, img: "Nintendo Switch", trailer: "x"},
+  {name: "Ninja Gaiden", release_date: 20150519, img: "Xbox", trailer: "x"}
 ])
